b4f5b5e5ce36e587b6e09db74e2d13f55ceed519
kamalwc <carmichaelkw@gmail.com>
2020-09-01 00:16:51 -0400

    fixed bugs

--- server.js
+++ server.js
@@ -2,9 +2,8 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
-const Workout = require("./models/Workout.js");
 const app = express();
 
 app.use(logger("dev"));
@@ -14,67 +13,13 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", { useNewUrlParser: true });
-
-// getlastworkouts
-app.get("/api/workouts", (req, res) => {
-  Workout.find({})
-    .then(dbworkout => {
-      res.json(dbworkout);
-    })
-    .catch(err => {
-      res.json(err);
-    });
+mongoose.connect("mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
 });
 
-// getworkouts in range 
-app.get("/api/workouts/range", (req, res) => {
-  Workout.find({})
-    .then(dbworkout => {
-      res.json(dbworkout);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
-
-//add excercise
-app.put("/api/workouts/:id", ({ body }, res) => {
-  Workout.create(body)
-    .then(dbworkout => {
-      res.json(dbworkout);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
-
-//createWorkout
-app.post("/api/workouts", ({ body }, res) => {
-  Workout.create(body)
-    .then(dbworkout => {
-      res.json(dbworkout);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
-
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + './public/index.html'));
-});
-
-app.get('/stats', function(req, res) {
-  res.sendFile(path.join(__dirname + './public/stats.html'));
-});
-
-app.get('/exercise', function(req, res) {
-  res.sendFile(path.join(__dirname + './public/exercise.html'));
-});
-
-app.get('/exercise?', function(req, res) {
-  res.sendFile(path.join(__dirname + './public/exercise.html'));
-});
+app.use(require("./routes/api.js"));
+app.use(require("./routes/view.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
